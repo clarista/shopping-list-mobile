ed47ce2ae996376e872ae445ee7294d81408b487
clarista <clarista05@gmail.com>
2023-11-01 15:55:07 +0700

    Selesai mengerjakan tutorial 6

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         // restart instead.
         //
         // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        // tested with just a hot reload..
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
        
         useMaterial3: true,
